968cef2ad50c600b140c4891d3bd57f5778d1d27
Jack Firth <jackhfirth@gmail.com>
2017-05-27 00:55:29 -0700

    Add combinator module
    Defines generic helpers for extending and combining expectations

---
+++ combinator.rkt
@@ -0,0 +1,50 @@
+#lang racket/base
+
+(require racket/contract)
+
+(provide
+ (contract-out
+  [expect/context (-> expectation? context? expectation?)]
+  [expect-map (-> expectation? (-> any/c any/c) expectation?)]
+  [expect-all (->* () #:rest (listof expectation?) expectation?)]
+  [expect-and (->* () #:rest (listof expectation?) expectation?)]
+  [expect-if (-> expectation? predicate/c expectation?)]))
+
+(require fancy-app
+         racket/list
+         racket/stream
+         "base.rkt")
+
+
+(define (expect/context exp ctxt)
+  (expectation
+   (λ (v)
+     (define (add-context flt)
+       (define new-ctxts (cons ctxt (fault-contexts flt)))
+       (fault #:summary (fault-summary flt)
+              #:expected (fault-expected flt)
+              #:actual (fault-actual flt)
+              #:contexts new-ctxts))
+     (map add-context (expectation-faults exp v)))))
+
+(define (expect-map exp f)
+  (expectation
+   (λ (v)
+     (expectation-faults exp (f v)))))
+
+(define (expect-all . exps)
+  (expectation
+   (λ (v)
+     (append-map (expectation-faults _ v) exps))))
+
+(define (expect-and . exps)
+  (expectation
+   (λ (v)
+     (define faults-stream (stream-map (expectation-faults _ v) exps))
+     (for/first ([faults (in-stream faults-stream)]
+                 #:unless (empty? faults))
+       faults))))
+
+(define (expect-if exp pred)
+  (expectation
+   (λ (v) (if (pred v) (expectation-faults exp v) (list)))))
