fd43bbc4d5813a4251b3672ca7d09d478567b9a5
Jack Firth <jackhfirth@gmail.com>
2017-05-27 20:13:36 -0700

    Move implementation modules into a private/ directory

--- main.rkt
+++ main.rkt
@@ -1,10 +1,2 @@
 #lang reprovide
-"base.rkt"
-"combinator.rkt"
-"convert.rkt"
-(except-in "data.rkt"
-           ;; omitted because convert.rkt reprovides these with logic handling
-           ;; expectation conversion
-           expect-equal?
-           expect-not-equal?
-           expect-list)
+"private/main.rkt"
