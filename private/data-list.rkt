d2307d1dfe6c5faf3894ec42663fcb22026c980d
Jack Firth <jackhfirth@gmail.com>
2017-06-07 18:25:08 -0700

    Add list expectation tests

--- private/data-list.rkt
+++ private/data-list.rkt
@@ -17,7 +17,8 @@
          "util.rkt")
 
 (module+ test
-  (require rackunit))
+  (require racket/function
+           rackunit))
 
 
 (define (expect-list-ref exp idx)
@@ -32,3 +33,13 @@
   (expect-and (expect-pred list?)
               (expect-all (expect-list-count (expect-equal? (length exps)))
                           (expect/dependent list->items-exp))))
+
+(module+ test
+  (define expect-num+sym
+    (expect-list (expect-pred number?) (expect-pred symbol?)))
+  (check-not-exn (thunk (expect! '(10 foo) expect-num+sym)))
+  (check-exn exn:fail:expect? (thunk (expect! '(10 20) expect-num+sym)))
+  (check-exn exn:fail:expect? (thunk (expect! '(foo bar) expect-num+sym)))
+  (check-exn exn:fail:expect? (thunk (expect! '(10) expect-num+sym)))
+  (check-exn exn:fail:expect? (thunk (expect! '(10 foo extra) expect-num+sym)))
+  (check-exn exn:fail:expect? (thunk (expect! 'not-list expect-num+sym))))
