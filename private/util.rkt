171f95b64ab2287795d503cab5a29861bfbf445f
Jack Firth <jackhfirth@gmail.com>
2017-06-08 12:56:00 -0700

    Add a list slicing utility

--- private/util.rkt
+++ private/util.rkt
@@ -3,9 +3,11 @@
 (provide rest->
          rest->*
          take/chop
-         map/index)
+         map/index
+         slice)
 
-(require racket/contract
+(require fancy-app
+         racket/contract
          racket/list)
 
 (module+ test
@@ -33,3 +35,16 @@
 
 (module+ test
   (check-equal? (map/index + '(10 10 10)) '(10 11 12)))
+
+(define (slice vs
+               #:start [start 0]
+               #:stop [stop (length vs)]
+               #:step [step 1])
+  (map (list-ref vs _) (range start stop step)))
+
+(module+ test
+  (check-equal? (slice '(a b c d e)) '(a b c d e))
+  (check-equal? (slice '(a b c d e) #:start 2) '(c d e))
+  (check-equal? (slice '(a b c d e) #:stop 2) '(a b))
+  (check-equal? (slice '(a b c d e) #:step 2) '(a c e))
+  (check-equal? (slice '(a b c d e) #:start 1 #:stop 4 #:step 2) '(b d)))
