869507179b98c6dce62970c285760004a4198bf3
Jack Firth <jackhfirth@gmail.com>
2017-06-07 19:06:11 -0700

    Add implementations of subset and superset expectations

--- private/data-set.rkt
+++ private/data-set.rkt
@@ -5,7 +5,9 @@
 (provide
  (contract-out
   [expect-set-member? (-> any/c expectation?)]
-  [expect-set-not-member? (-> any/c expectation?)]))
+  [expect-set-not-member? (-> any/c expectation?)]
+  [expect-subset (-> set? expectation?)]
+  [expect-superset (-> set? expectation?)]))
 
 (require racket/set
          "base.rkt"
@@ -54,3 +56,23 @@
              (thunk (expect! (set 1 2 3) (expect-set-not-member? 1))))
   (check-exn #rx"expected: not set containing 1"
              (thunk (expect! (set 1 2 3) (expect-set-not-member? 1)))))
+
+(define (expect-subset big-st)
+  (define (make-expectation-from-extras little-st)
+    (apply expect-all
+           (map expect-set-not-member?
+                (set->list (set-subtract little-st big-st)))))
+  (expect/dependent make-expectation-from-extras))
+
+(module+ test
+  (check-not-exn (thunk (expect! (set 1 2) (expect-subset (set 1 2 3)))))
+  (check-exn #rx"multiple failures"
+             (thunk (expect! (set 1 2 'foo 'bar) (expect-subset (set 1 2 3))))))
+
+(define (expect-superset little-st)
+  (apply expect-all (map expect-set-member? (set->list little-st))))
+
+(module+ test
+  (check-not-exn (thunk (expect! (set 1 2 3) (expect-superset (set 1 2)))))
+  (check-exn #rx"multiple failures"
+             (thunk (expect! (set 1) (expect-superset (set 1 2 3))))))
