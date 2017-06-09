04c544a3b0047751f56703d66395fa8a24243226
Jack Firth <jackhfirth@gmail.com>
2017-06-08 22:11:43 -0700

    Split conversion core logic from wrapper logic and add tests

--- private/convert.rkt
+++ private/convert.rkt
@@ -2,12 +2,11 @@
 
 (require racket/contract)
 
-(provide expect-eq?
-         (contract-out
-          [expectation-convert (-> expectation-convertible? expectation?)]
-          [expectation-convertible? predicate/c]
+(provide (contract-out
           [rename expect-equal?/convert expect-equal?
-                  (-> any/c expectation?)]))
+                  (-> any/c expectation?)]
+          [rename expect-hash/convert expect-hash
+                  (rest->* (list any/c cvrt?) exp?)]))
 
 (require (for-syntax racket/base
                      racket/syntax)
@@ -18,6 +17,7 @@
          syntax/parse/define
          "base.rkt"
          "compare.rkt"
+         "convert-base.rkt"
          "data-hash.rkt"
          "data-list.rkt"
          "data-set.rkt"
@@ -30,38 +30,8 @@
   (require rackunit))
 
 
-(define expectation-convertible?
-  (or/c expectation?
-        list? vector? set? hash?
-        boolean? number? string? symbol? char?))
-
-(define (expectation-convert v)
-  (cond
-    [(expectation? v) v]
-    [(list? v) (apply expect-list (map expectation-convert v))]
-    [(vector? v)
-     (apply expect-vector (map expectation-convert (vector->list v)))]
-    [(set? v) (apply expect-set (set->list v))]
-    [(hash? v)
-     (define converted (map expectation-convert (hash-values v)))
-     (apply expect-hash (append-map list (hash-keys v) converted))]
-    [(boolean? v) (if v expect-true expect-false)]
-    [((disjoin number? string? symbol? char?) v) (expect-equal? v)]))
-
-(module+ test
-  (check-pred expectation? (expectation-convert 'foo))
-  (check-pred expectation? (expectation-convert #t))
-  (check-pred expectation? (expectation-convert expect-true))
-  (check-pred expectation? (expectation-convert (list 1 2 3)))
-  (check-pred expectation? (expectation-convert (vector 1 2 3)))
-  (check-pred expectation? (expectation-convert (hash 'a 1)))
-  (check-pred expectation? (expectation-convert (set 1 2 3)))
-  (check-pred expectation?
-              (expectation-convert
-               (hash 'a (list 1 2 3)
-                     'b (vector "foo" "bar")
-                     'c (set 'a 'b 'c)
-                     'd #f))))
+(define exp? expectation?)
+(define cvrt? expectation-convertible?)
 
 (define (expect-equal?/convert v)
   (cond
@@ -74,40 +44,9 @@
      (apply expect-hash (append-map list (hash-keys v) converted))]
     [else (expect-equal? v)]))
 
-(define-syntax <convert> #f)
-
-(begin-for-syntax
-  (define-syntax-class arg-id
-    #:literals (<convert>)
-    (pattern <convert>
-             #:attr id (generate-temporary)
-             #:attr expr #'(expectation-convert id))
-    (pattern id:id #:attr expr #'id)))
-
-(define-syntax-parser define/expectation-conversion
-  [(_ (id:id arg:arg-id ... . rest-arg:arg-id) contract-expr:expr)
-   #'(begin
-       (define (converted arg.id ... . rest-arg.id)
-         (apply id arg.expr ... (map expectation-convert rest-arg.id)))
-       (provide (contract-out (rename converted id contract-expr))))]
-  [(_ (id:id arg:arg-id ...) contract-expr:expr)
-   #'(begin
-       (define (converted arg.id ...) (id arg.expr ...))
-       (provide (contract-out (rename converted id contract-expr))))])
-
-(define-simple-macro (define-conversions [header contract] ...)
-  (begin (define/expectation-conversion header contract) ...))
-
-(define exp? expectation?)
-(define cvrt? expectation-convertible?)
-
 (define (expect-hash/convert . k+vs)
   (define converted (map expectation-convert (slice k+vs #:start 1 #:step 2)))
   (apply expect-hash (append-map list (slice k+vs #:step 2) converted)))
-
-(provide
- (contract-out
-  [rename expect-hash/convert expect-hash (rest->* (list any/c cvrt?) exp?)]))
 
 (define-conversions
   [(expect-all . <convert>) (rest-> cvrt? exp?)]
