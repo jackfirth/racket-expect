0e7b27e05172ed154ccd8a02f04bff43815103df
Jack Firth <jackhfirth@gmail.com>
2017-09-15 15:54:02 -0500

    Add tests for #:contexts argument of expect-fault

--- tests/meta.rkt
+++ tests/meta.rkt
@@ -9,16 +9,21 @@
 (struct test-attribute attribute () #:transparent)
 (define test-attr (test-attribute "test attr"))
 
+(struct test-context context () #:transparent)
+(define test-ctxt (test-context "test ctxt"))
 
 (define test-fault
   (fault #:summary "some fault"
          #:actual (make-self-attribute 'foo)
-         #:expected test-attr))
+         #:expected test-attr
+         #:contexts (list test-ctxt)))
 
 (test-case "expect-fault"
   (check-expect test-fault (expect-fault))
   (check-expect test-fault (expect-fault #:summary "some fault"))
   (check-expect test-fault (expect-fault #:actual (expect-pred self-attribute?)))
   (check-expect test-fault (expect-fault #:expected test-attr))
+  (check-expect test-fault (expect-fault #:contexts (list test-ctxt)))
+  (check-expect test-fault (expect-fault #:contexts (expect-pred list?)))
   (check-expect (expect-fault #:summary "some other fault")
                 (expect-exp-faults test-fault expect-any)))
