a4c5329eab1afb43f895c266bfd6e274bbccb14a
Jack Firth <jackhfirth@gmail.com>
2017-09-15 03:38:38 -0500

    Replace key-set-context with expect-apply use

--- tests/data/data-hash.rkt
+++ tests/data/data-hash.rkt
@@ -1,30 +1,32 @@
 #lang racket/base
 
 (require expect
+         expect/rackunit
          fancy-app
          racket/function
-         rackunit)
+         racket/set
+         (only-in rackunit test-case))
 
 
-(check-not-exn
- (thunk (expect! (hash 'a 1 'b 2) (expect-hash-count (expect-equal? 2)))))
+(test-case "expect-hash-count"
+  (check-expect (expect-hash-count 2) (expect-exp-faults (hash 'a 1 'b 2))))
 
-(check-exn #rx"in: value for key 'foo"
-           (thunk (expect! (hash 'foo 1)
-                           (expect-hash-ref 'foo (expect-equal? 2)))))
+(test-case "expect-hash-ref"
+  (define foo-ref-exp (expect-hash-ref 'foo 1))
+  (check-expect foo-ref-exp (expect-exp-faults (hash 'foo 1)))
+  (define ctxt-exp
+    (expect-struct context [context-description "value for key 'foo"]))
+  (define fault-exp (expect-fault #:contexts (list ctxt-exp)))
+  (check-expect foo-ref-exp (expect-exp-faults (hash 'foo 2) fault-exp)))
 
-(check-not-exn
- (thunk (expect! (hash 'foo 1) (expect-hash-keys (expect-set-member? 'foo)))))
-(check-exn #rx"in: the set of hash keys"
-           (thunk (expect! (hash 'foo 1)
-                           (expect-hash-keys (expect-set-member? 'bar)))))
+(test-case "expect-hash-keys"
+  (check-expect (expect-hash-keys (set 'foo))
+                (expect-exp-faults (hash 'foo 1))))
 
-(define expect-foo1-bar2!
-  (expect! _ (expect-hash 'foo (expect-equal? 1) 'bar (expect-equal? 2))))
-(check-not-exn  (thunk (expect-foo1-bar2! (hash 'foo 1 'bar 2))))
-(check-exn exn:fail:expect?
-           (thunk (expect-foo1-bar2! (hash 'foo 1 'bar 2 'baz 3))))
-(check-exn exn:fail:expect?
-           (thunk (expect-foo1-bar2! (hash 'foo 1))))
-(check-exn exn:fail:expect?
-           (thunk (expect-foo1-bar2! (hash 'foo 1 'bar 5))))
+(test-case "expect-hash"
+  (define hash-exp (expect-hash 'foo 1 'bar 2))
+  (check-expect hash-exp (expect-exp-faults (hash 'foo 1 'bar 2)))
+  (check-expect hash-exp (expect-exp-faults (hash 'foo 1) expect-any))
+  (check-expect hash-exp
+                (expect-exp-faults (hash 'foo 1 'bar 2 'extra 3) expect-any))
+  (check-expect hash-exp (expect-exp-faults (hash 'foo 1 'bar 100) expect-any)))
