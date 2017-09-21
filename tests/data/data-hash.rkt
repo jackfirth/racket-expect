2bde89f22b3bb61c528ca6582d1925ec641d752e
Jack Firth <jackhfirth@gmail.com>
2017-09-20 21:38:46 -0500

    Use meta expectations in hash tests

--- tests/data/data-hash.rkt
+++ tests/data/data-hash.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
-(require expect
+(require arguments
+         expect
          expect/rackunit
          fancy-app
          racket/function
@@ -9,24 +10,70 @@
 
 
 (test-case "expect-hash-count"
-  (check-expect (expect-hash-count 2) (expect-exp-faults (hash 'a 1 'b 2))))
+  (test-subject #:subject (expect-hash-count (expect-pred even?))
+    (expect-exp-faults (hash 'a 1 'b 2))
+    (expect-exp-faults (hash))
+    (expect-exp-faults (hash 'a 1 'b 2 'c 3)
+                       (expect-fault #:expected (make-pred-attribute even?)
+                                     #:actual (make-self-attribute 3)
+                                     #:contexts (list the-length-context))))
+  (test-subject "conversion" #:subject (expect-hash-count 2)
+    (expect-exp-faults (hash 'a 1 'b 2))
+    (expect-exp-faults (hash 'a 1) expect-any))
+  (test-subject "contract" #:subject expect-hash-count
+    (expect-call-exn (arguments 'foo) #rx"contract")))
 
 (test-case "expect-hash-ref"
-  (define foo-ref-exp (expect-hash-ref 'foo 1))
-  (check-expect foo-ref-exp (expect-exp-faults (hash 'foo 1)))
-  (define ctxt-exp
-    (expect-struct context [context-description "value for key 'foo"]))
-  (define fault-exp (expect-fault #:contexts (list ctxt-exp)))
-  (check-expect foo-ref-exp (expect-exp-faults (hash 'foo 2) fault-exp)))
+  (test-subject #:subject (expect-hash-ref 'foo (expect-pred positive?))
+    (expect-exp-faults (hash 'foo 10 'bar 2))
+    (expect-exp-faults (hash 'foo 10 'bar -3))
+    (expect-exp-faults (hash 'foo 10))
+    (expect-exp-faults (hash 'foo -10)
+                       (expect-fault #:expected (make-pred-attribute positive?)
+                                     #:actual (make-self-attribute -10)
+                                     #:contexts (list
+                                                 (make-dict-context 'foo)))))
+  (test-subject "conversion" #:subject (expect-hash-ref 'foo '(1 2 3))
+    (expect-exp-faults (hash 'foo '(1 2 3) 'bar '(4 5 6)))
+    (expect-exp-faults (hash 'foo '(1 2 10)) expect-any)))
 
 (test-case "expect-hash-keys"
-  (check-expect (expect-hash-keys (set 'foo))
-                (expect-exp-faults (hash 'foo 1))))
+  (define keys-length-contexts (list the-keys-context the-length-context))
+  (test-subject #:subject (expect-hash-keys (expect-set-count 2))
+    (expect-exp-faults (hash 'a 1 'b 2))
+    (expect-exp-faults (hash 'a 1)
+                       (expect-fault #:expected (make-equal-attribute 2)
+                                     #:actual (make-self-attribute 1)
+                                     #:contexts keys-length-contexts)))
+  (test-subject "conversion" #:subject (expect-hash-keys (set 'a 'b))
+    (expect-exp-faults (hash 'a 1 'b 2))
+    (expect-exp-faults (hash 'a 1) expect-any))
+  (test-subject "contract" #:subject expect-hash-keys
+    (expect-call-exn (arguments '(a b)) #rx"contract")))
 
 (test-case "expect-hash"
-  (define hash-exp (expect-hash 'foo 1 'bar 2))
-  (check-expect hash-exp (expect-exp-faults (hash 'foo 1 'bar 2)))
-  (check-expect hash-exp (expect-exp-faults (hash 'foo 1) expect-any))
-  (check-expect hash-exp
-                (expect-exp-faults (hash 'foo 1 'bar 2 'extra 3) expect-any))
-  (check-expect hash-exp (expect-exp-faults (hash 'foo 1 'bar 100) expect-any)))
+  (test-subject #:subject (expect-hash 'a expect-any 'b (expect-equal? 2))
+    (expect-exp-faults (hash 'a 1 'b 2))
+    (expect-exp-faults (hash 'a 1 'b 100)
+                       (expect-fault #:contexts (list (make-dict-context 'b))))
+    (expect-exp-faults (hash 'a 100 'b 2))
+    (expect-exp-faults (hash 'a 1)
+                       (expect-fault
+                        #:expected
+                        (make-contains-all-attribute set-member? '(b))
+                        #:actual (make-self-attribute (set 'a))
+                        #:contexts (list the-keys-context)))
+    (expect-exp-faults (hash 'a 1 'b 2 'c 3)
+                       (expect-fault
+                        #:expected
+                        (make-contains-none-attribute set-member? '(c))
+                        #:actual (make-self-attribute (set 'a 'b 'c))
+                        #:contexts (list the-keys-context))))
+  (test-subject "conversion" #:subject (expect-hash 'a '(1 2 3))
+    (expect-exp-faults (hash 'a '(1 2 3)))
+    (expect-exp-faults (hash 'a '(1 2 100))
+                       (expect-fault
+                        #:contexts (list (make-dict-context 'a)
+                                         (make-sequence-context 2)))))
+  (test-subject "contract" #:subject expect-hash
+    (expect-call-exn (arguments 'a 1 'b) #rx"contract")))
